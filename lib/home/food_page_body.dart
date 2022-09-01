284ad52566842904964680d47f56328803b2dab5
Nomanshariq562 <86268414+Nomanshariq562@users.noreply.github.com>
2022-09-01 16:13:44 +0500

    Updated

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -3,12 +3,35 @@ import 'package:food_delivery_app/widgets/big_text.dart';
 import 'package:food_delivery_app/widgets/icon_and_text_widget.dart';
 import 'package:food_delivery_app/widgets/small_text.dart';
 
-class FoodPageBody extends StatelessWidget {
+class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
 
   @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    PageController pageController = PageController(viewportFraction: 0.85);
     return Container(
       height: 320,
       child: PageView.builder(
@@ -21,101 +44,144 @@ class FoodPageBody extends StatelessWidget {
   }
 
   Widget _buildPagerItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(
-            left: 10,
-            right: 10,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(
-                "assets/image/foodpro2.jpg",
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  "assets/image/foodpro2.jpg",
+                ),
               ),
             ),
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 130,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 26),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(35),
-              color: Colors.white,
-            ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              padding: EdgeInsets.only(left: 22, right: 20, top: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "New York-Style Pizza."),
-                  SizedBox(
-                    height: 10,
+              height: 130,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 26),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5), // changes position of shadow
                   ),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(
-                            5,
-                            (index) => Icon(
-                                  Icons.star,
-                                  color: Colors.cyan,
-                                  size: 20,
-                                )),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(text: "4.5"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(text: "1287"),
-                      SizedBox(
-                        width: 6,
-                      ),
-                      SmallText(text: "comments"),
-                      SizedBox(
-                        height: 30,
-                      ),
-                    ],
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5, 0), // changes position of shadow
                   ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 40,
-                      ),
-                      IconAndText(
-                          icon: Icons.circle_rounded,
-                          iconColour: Color.fromARGB(255, 247, 194, 116),
-                          text: "Normal"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      IconAndText(
-                          icon: Icons.location_pin,
-                          iconColour: Colors.cyan,
-                          text: "1.7m"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      IconAndText(
-                          icon: Icons.timelapse,
-                          iconColour: Colors.red,
-                          text: "32min")
-                    ],
-                  )
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, 0), // changes position of shadow
+                  ),
                 ],
               ),
+              child: Container(
+                padding: EdgeInsets.only(left: 22, right: 20, top: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "New York-Style Pizza."),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                              5,
+                              (index) => Icon(
+                                    Icons.star,
+                                    color: Colors.cyan,
+                                    size: 20,
+                                  )),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1287"),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        SmallText(text: "comments"),
+                        SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          height: 40,
+                        ),
+                        IconAndText(
+                            icon: Icons.circle_rounded,
+                            iconColour: Color.fromARGB(255, 247, 194, 116),
+                            text: "Normal"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        IconAndText(
+                            icon: Icons.location_pin,
+                            iconColour: Colors.cyan,
+                            text: "1.7m"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        IconAndText(
+                            icon: Icons.timelapse,
+                            iconColour: Colors.red,
+                            text: "32min")
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
